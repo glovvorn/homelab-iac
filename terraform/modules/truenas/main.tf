c74879970685adb5fe68da2cfb8804d04252faac
Greg Lovvorn <greg.lovvorn@gmail.com>
2025-05-07 11:17:11 -0400

    Fix truenas module, add prevent_destory to datasets

--- terraform/modules/truenas/main.tf
+++ terraform/modules/truenas/main.tf
@@ -1,15 +1,19 @@
 resource "truenas_dataset" "datasets" {
-  for_each = { for dataset in var.datasets : dataset.name => dataset }
+  for_each = { for ds in var.datasets : ds.name => ds }
 
-  name = each.value.name
-  pool = "data"
-  path = each.value.path
+  pool = "data"          # the ZFS pool name
+  name = each.value.name # the dataset name within that pool
+
+  lifecycle {
+    prevent_destroy = true # Prevent accidental deletion
+  }
 }
 
 resource "truenas_share_nfs" "nfs_shares" {
-  for_each = { for dataset in var.datasets : dataset.name => dataset }
+  for_each = { for ds in var.datasets : ds.name => ds }
 
-  path    = truenas_dataset.datasets[each.key].path
-  enabled = true
-  hosts   = each.value.nfs_hosts
+  paths = [
+    truenas_dataset.datasets[each.key].path
+  ]
+  hosts = each.value.nfs_hosts
 }
