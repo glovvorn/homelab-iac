dfffebfe11335904530baece39c05ac4cae933e1
Greg Lovvorn <greg.lovvorn@gmail.com>
2025-05-07 10:16:40 -0400

    Add per-module providers file

---
+++ terraform/modules/truenas/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "~> 2.9.0"
+    }
+    truenas = {
+      source  = "dariusbakunas/truenas"
+      version = "~> 0.11.0"
+    }
+  }
+}
