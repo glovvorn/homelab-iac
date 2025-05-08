3b969275d8ee22d200aab4500bac547fe1d0bbb0
Greg Lovvorn <greg.lovvorn@gmail.com>
2025-05-08 15:53:38 -0400

    Add key to var def

--- terraform/modules/truenas/variables.tf
+++ terraform/modules/truenas/variables.tf
@@ -1,6 +1,7 @@
 variable "datasets" {
   description = "List of datasets and NFS configurations"
   type = list(object({
+    key       = string
     name      = string
     path      = string
     nfs_hosts = list(string)
